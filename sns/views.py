e71aec9d0882de64bea44ab9936b684bd9b95ebf
fujitoshion <shion@shion.local>
2020-10-18 23:11:54 +0900

    static folder

--- sns/views.py
+++ sns/views.py
@@ -3,10 +3,29 @@ from django.http import HttpResponse#アクセスに送り返すもの
 
 
 def index(request):
-    if 'msg' in request.GET:
-        msg = request.GET['msg']
-        result = 'you typed:' + msg
-    else:
-        result = 'please send msg'
-    return HttpResponse(result)
+    params = {
+        'title':'sns/index',
+        'msg':'お名前は？',
+        # 'goto':'next',
+    }
+    return render(request, 'sns/index.html', params)
+
+
+# def next(request):
+    # params = {
+    #     'title':'sns/next',
+    #     'msg':'next page',
+    #     'goto':'index',
+    # }
+    # return render(request, 'sns/index.html', params)
+
+
+def form(request):
+    msg = request.POST['msg']
+    params = {
+        'title':'sns/form',
+        'msg':'こんにちは、'+msg+'さん',
+    }
+    return render(request, 'sns/index.html', params)
+
 
