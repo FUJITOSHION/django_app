af0f892af7773a89a629664cafe8152c25a5ce3b
fujitoshion <shion@shion.local>
2020-10-18 15:06:46 +0900

    [add] rooting

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # djangoの勉強
 - READMEには開発のMEMOを書く
+
+## ディレクトリー構成
+- configはプロジェックト関連
+- snsはapp関連
--- sns/views.py
+++ sns/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse#アクセスに送り返すもの
 
-# Create your views here.
+
+def index(request):
+    if 'msg' in request.GET:
+        msg = request.GET['msg']
+        result = 'you typed:' + msg
+    else:
+        result = 'please send msg'
+    return HttpResponse(result)
+
