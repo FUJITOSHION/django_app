81980cca5e3566bf16a81cef329c949adc8d23fa
fujitoshion <shion@shion.local>
2020-10-29 21:33:51 +0900

    models

--- sns/forms.py
+++ sns/forms.py
@@ -2,6 +2,12 @@ from django import forms
 
 
 class SnsForm(forms.Form):
-    name = forms.CharField(label='name')
-    mail = forms.CharField(label='mail')
-    age = forms.IntegerField(label='age')
+    data = [
+        ('one', 'item1'),
+        ('two', 'item2'),
+        ('three', 'item3'),
+    ]
+    check = forms.ChoiceField(label='ratio',choices=data, widget=forms.RadioSelect())
+    # name = forms.CharField(label='name')
+    # mail = forms.CharField(label='mail')
+    # age = forms.IntegerField(label='age')
--- sns/views.py
+++ sns/views.py
@@ -1,38 +1,14 @@
+from sns.models import Friend
 from django.shortcuts import render
 from django.http import HttpResponse#アクセスに送り返すもの
 from .forms import SnsForm
-
+from django.views.generic import TemplateView
 
 def index(request):
-    params = {
-        'title':'sns/index',
-        'msg':'お名前は？',
-        'form':SnsForm(),
+    data = Friend.objects.all()
+    params ={
+        'title':'Hello',
+        'message':'all freineds',
+        'data':data,
     }
-    if (request.method == 'POST'):
-        params['message'] = 'name' + request.POST['name'] +\
-            '<br> mail' + request.POST['mail'] + \
-            '<br> age' + request.POST['age']
-        
-        params['form'] = SnsForm(request.POST)
     return render(request, 'sns/index.html', params)
-
-
-# def next(request):
-    # params = {
-    #     'title':'sns/next',
-    #     'msg':'next page',
-    #     'goto':'index',
-    # }
-    # return render(request, 'sns/index.html', params)
-
-
-# def form(request):
-#     msg = request.POST['msg']
-#     params = {
-#         'title':'sns/form',
-#         'msg':'こんにちは、'+msg+'さん',
-#     }
-#     return render(request, 'sns/index.html', params)
-
-
